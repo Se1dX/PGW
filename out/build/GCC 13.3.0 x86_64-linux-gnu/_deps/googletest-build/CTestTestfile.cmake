de050c7cd1641cd8dca1fe200cdd8203460b509f
Se1dX <mkntsh.ltk@gmail.com>
2025-07-25 02:12:34 +0300

    feature: добавлен основной CmakeLists и структура проекта

---
+++ out/build/GCC 13.3.0 x86_64-linux-gnu/_deps/googletest-build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/se1d/PGW/out/build/GCC 13.3.0 x86_64-linux-gnu/_deps/googletest-src
+# Build directory: /home/se1d/PGW/out/build/GCC 13.3.0 x86_64-linux-gnu/_deps/googletest-build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("googlemock")
